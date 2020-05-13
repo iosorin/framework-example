2f7ee1ffd9669c96455ef4d629e29f7e256aa802
iosorin <miroslav.osorin@gmail.com>
2020-05-13 09:48:33 +0200

    Формирование верстки, + dom.js библиотека(остновилась на on() method)

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,21 @@
 import { ExcelComponent } from '@/core/ExcelComponent';
 
 export class Header extends ExcelComponent {
+    static className = 'excel__header';
+
     toHtml() {
-        return 'Header';
+        return `
+            <input type="text" class="input" value="Новая таблица" />
+
+            <div>
+                <div class="button">
+                    <i class="material-icons">delete</i>
+                </div>
+
+                <div class="button">
+                    <i class="material-icons">exit_to_app</i>
+                </div>
+            </div>
+        `;
     }
 }
