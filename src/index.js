018cea1d16bf7448ca6fa72ca23c73e0695cc44d
osorina <official.osorina@gmail.com>
2020-05-13 09:12:03 +0200

    DomListener, ExcelComponent, вывод компонентов в Excel.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import './scss/index.scss';
-console.log('bla');
+
+import { Excel } from '@/components/excel/Excel';
+
+import { Formula } from '@/components/formula/Formula';
+import { Header } from '@/components/header/Header';
+import { Table } from '@/components/table/Table';
+import { Toolbar } from '@/components/toolbar/Toolbar';
+
+const excel = new Excel('#app', {
+    components: [Formula, Header, Table, Toolbar]
+});
+
+excel.render();
