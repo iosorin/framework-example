85e766605f855a153a461130560bd8e8b2cc6392
osorina <official.osorina@gmail.com>
2020-05-13 14:44:35 +0200

    + LISTENERS (add, remove, bind)

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -3,6 +3,20 @@ import { ExcelComponent } from '@/core/ExcelComponent';
 export class Toolbar extends ExcelComponent {
     static className = 'excel__toolbar';
 
+    constructor($root) {
+        const name = 'Toolbar';
+        const listeners = ['click'];
+
+        super($root, {
+            name,
+            listeners
+        });
+    }
+
+    onClick(evt) {
+        console.log('Formula: onClick', evt.target, this.$root);
+    }
+
     toHtml() {
         return `
             <div class="button">
--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,9 +1,46 @@
+import { capitalize } from '@core/utils';
+
 export class DomListener {
-    constructor($root) {
+    constructor($root, listeners = []) {
         if (!$root) {
             throw new Error('No $root provided for DomListener!');
         }
 
         this.$root = $root;
+        this.listeners = listeners;
     }
+
+    initDOMListeners() {
+        if (!Array.isArray(this.listeners) || !this.listeners.length) {
+            return;
+        }
+
+        this.listeners.forEach((listener) => {
+            const method = getMethodName(listener);
+
+            // Method is not implemented
+            if (!this[method]) {
+                throw new Error(`Method ${ method } is not implemented in ${ this.name } Component`);
+            }
+
+            // Bind context in this way so that you can actually REMOVE event listener later
+            this[method] = this[method].bind(this);
+
+            // Same as addEventListener
+            this.$root.on(listener, this[method]);
+        });
+    }
+
+    removeDOMListeners() {
+        this.listeners.forEach((listener) => {
+            const method = getMethodName(listener);
+
+            // Same as addEventListener
+            this.$root.off(listener, this[method]);
+        });
+    }
+}
+
+function getMethodName(eventName) {
+    return `on${ capitalize(eventName) }`;
 }
