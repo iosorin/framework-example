3323f2f19c3690f33f1b7e1fc722f0228f00f0e9
osorina <official.osorina@gmail.com>
2020-05-20 23:25:13 +0200

    StoreSubscriber (subcribe on 'example'), watch changes

---
+++ src/core/StoreSubscriber.js
@@ -0,0 +1,38 @@
+import { isEqual } from '@core/utils';
+
+export class StoreSubscriber {
+    constructor(store) {
+        this.store = store;
+        this.sub = null;
+
+        this.prevState = {};
+    }
+
+    subscribeComponents(components) {
+        this.prevState = this.store.getState();
+
+        this.sub = this.store.subscribe((state) => {
+            Object
+                .keys(state)
+                .forEach((key) => {
+                    const hasChanges = !isEqual(this.prevState[key], state[key]);
+
+                    if (hasChanges) {
+                        components.forEach((component) => {
+                            if (component.isWatching(key)) {
+                                const changes = { [key]: state[key] };
+
+                                component.storeChanged(changes);
+                            }
+                        });
+                    }
+                });
+
+            this.prevState = this.store.getState();
+        });
+    }
+
+    unsubscribeFromStore() {
+        this.sub.unsubscribe();
+    }
+}
