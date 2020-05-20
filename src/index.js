1f29114a31bb1995d84128a052ccf59bbab044ea
iosorin <miroslav.osorin@gmail.com>
2020-05-20 18:08:52 +0200

    createStore (observer again), rootReducer mockup

--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@ import { Header } from '@/components/header/Header';
 import { Table } from '@/components/table/Table';
 import { Toolbar } from '@/components/toolbar/Toolbar';
 
+import { createStore } from '@core/createStore';
+import { rootReducer } from '@store/rootReducer';
+
+const store = createStore(rootReducer);
+
 const excelApp = new Excel('#app', {
+    store,
     components: [Formula, Header, Table, Toolbar]
 });
 
--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,8 @@ module.exports = {
         extensions: ['.js'],
         alias: {
             '@': path.resolve(__dirname, 'src'),
-            '@core': path.resolve(__dirname, 'src/core')
+            '@core': path.resolve(__dirname, 'src/core'),
+            '@store': path.resolve(__dirname, 'src/store')
         }
     },
     devtool: isDev ? 'source-map' : false,
