dcd728c8a96976c3ce56186ecf6a19d0ec8294a7
osorina <official.osorina@gmail.com>
2020-05-20 07:28:52 +0200

    + observer

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,9 +1,11 @@
 import { $ } from '@core/dom';
+import { Observer } from '@core/Observer';
 
 export class Excel {
     constructor(selector, options) {
         this.$el = $(selector);
 
+        this.observer = new Observer();
         this.components = options.components || [];
     }
 
@@ -11,13 +13,18 @@ export class Excel {
         // Create root Dom instancte
         const $root = $.create('div', 'excel');
 
+        // Component options
+        const options = {
+            observer: this.observer
+        };
+
         // Waklhrough all passed components
         this.components = this.components.map((Component) => {
             // Create El instance with a passed static className
             const $el = $.create('div', Component.className);
 
             // Pass it to DomListener constructor
-            const component = new Component($el);
+            const component = new Component($el, options);
 
             // Append component inner html
             $el.html(component.toHtml());
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,6 +5,7 @@ export class ExcelComponent extends DomListener {
         super($root, options.listeners);
 
         this.name = options.name || '';
+        this.observer = options.observer;
 
         this.onBeforeInit?.();
     }
