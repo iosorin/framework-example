78a123a87a5ad1b456776ce737b8f902fa659a73
iosorin <miroslav.osorin@gmail.com>
2020-05-21 07:30:07 +0200

    Router Base

--- src/app.js
+++ src/app.js
@@ -1,33 +1,37 @@
 import './scss/index.scss';
+import { Router } from '@core/routes/Router';
 
-// Components
-import { Excel } from '@/components/excel/Excel'; // root
-import { Formula } from '@/components/formula/Formula';
-import { Header } from '@/components/header/Header';
-import { Table } from '@/components/table/Table';
-import { Toolbar } from '@/components/toolbar/Toolbar';
+new Router('#app', {
 
-// @core
-import { createStore } from '@core/createStore';
-import { storage, debounce } from '@core/utils';
-
-// @store
-import { rootReducer } from '@store/rootReducer';
-import { initialState } from '@store/initialState';
-
-
-// Create debounced store.state listener
-const store = createStore(rootReducer, initialState);
-
-const stateListener = debounce((state) => {
-    storage('App-State', state);
-}, 300);
-
-store.subscribe(stateListener);
-
-const excelApp = new Excel('#app', {
-    store,
-    components: [Formula, Header, Table, Toolbar]
 });
+// // Components
+// import { Excel } from '@/components/excel/Excel'; // root
+// import { Formula } from '@/components/formula/Formula';
+// import { Header } from '@/components/header/Header';
+// import { Table } from '@/components/table/Table';
+// import { Toolbar } from '@/components/toolbar/Toolbar';
 
-excelApp.render();
+// // @core
+// import { createStore } from '@core/createStore';
+// import { storage, debounce } from '@core/utils';
+
+// // @store
+// import { rootReducer } from '@store/rootReducer';
+// import { initialState } from '@store/initialState';
+
+
+// // Create debounced store.state listener
+// const store = createStore(rootReducer, initialState);
+
+// const stateListener = debounce((state) => {
+//     storage('App-State', state);
+// }, 300);
+
+// store.subscribe(stateListener);
+
+// const excelApp = new Excel('#app', {
+//     store,
+//     components: [Formula, Header, Table, Toolbar]
+// });
+
+// excelApp.render();
---
+++ src/pages/DashboardPage.js
@@ -0,0 +1,7 @@
+import { Page } from '@core/Page';
+
+export class DashboardPage extends Page {
+    getRoot() {
+        return '<h1>Dashboard Page</h1>';
+    }
+}
